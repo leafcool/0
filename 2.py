d89fa73384050f8eb407bc6fc30b83ed51ec28a2
leafcool <leafcool@live.comlive.com>
2020-07-02 21:38:30 +0800

    Initial commit

---
+++ 2.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @FileName  :2.py
+# @Time      :2020/7/2 15:55
+# @Author    :leafcool
+
+
+# my_name='leafcool'
+# my_age=35#
+# my_height=74
+# my_wight=180
+# my_eyes='blue'
+# my_teeth='white'
+# my_hair='black'
+#
+# print ("lesjhjhjh %s."%my_name)
+# print("shjhjh%d."%my_age)
+# print("shjjhk %d"%my_wight)
+# print("Actually that's not too heavy")
+# print("He's got %s eyes and %s hair."%(my_eyes,my_hair))
+# print("His teeth are usually %s depending on the coffee." %my_teeth)
+#
+# print("if I add %d,%d,and %d I get %d."%(my_age,my_height,my_wight,my_age+my_height))
+
+x="there are %d types of people."%10
+binary="binary"
+do_not="don't"
+y="those who know %s and those who %s."%(binary,do_not)
+
+print(x)
+print(y)
+
+print("I said :%r."%x)
+print("I also said:'%s'"%y)
+
+hilarious=False
+joke_evaluation="isnot that j'dd'oke so funny/！%r"
+w="hsjhjdjjj"
+e="sjkkkj"
+print(w+e+w)
+print(joke_evaluation)
+
+print("mary had a little lamb")
+print("It's fleece was white as %s."%'snow')
+print("add everywhere that mary went")
+print(".")
+
+end1='1'
+end2='2'
+end3='3'
+end4='5'
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 ='fg'
+print(end1+end2+end3+end4+end5+end7+end8+end9+end12)
+url='https://github.com/521xueweihan/python'
