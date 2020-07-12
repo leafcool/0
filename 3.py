4015580898bed4327e84102a02f49748a8cc37e0
leafcool <leafcool@live.com>
2020-07-12 19:28:24 +0800

    fix not show bug

---
+++ 3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""
+@author: leafcool
+@contact:598115455@qq.com
+@version: 1.0.0
+@license: Apache Licence
+@file: 3.py
+@time: 2020/7/12 19:27
+"""
