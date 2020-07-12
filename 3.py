855620a2e08fff12b524abcb48895a6dab953831
leafcool <leafcool@live.comlive.com>
2020-07-12 17:30:06 +0800

    Initial commit

--- 3.py
+++ 3.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-# @FileName  :3.py
-# @Time      :2020/7/12 13:35
-# @Author    :leafcool
 
-#leafcool
+"""
+@author: leafcool
+@contact:598115455@qq.com
+@version: 1.0.0
+@license: Apache Licence
+@file: 3.py
+@time: 2020/7/12 19:27
+"""
 
-
+#shhh
